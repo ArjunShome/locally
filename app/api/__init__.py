c25cecdf94cc27a2b7262fbe10b7e30b8c93b05f
Arjun Shome <arjun_shome@epam.com>
2025-12-04 13:06:10 +0530

    Updated Repository structure

---
+++ app/api/__init__.py
@@ -0,0 +1,3 @@
+from Repository.locally.backend.app.api.v1.endpoints.login_apis import login_routes
+from Repository.locally.backend.app.api.v1.endpoints.report_apis import report_routes
+from Repository.locally.backend.app.api.v1.endpoints.user_apis import user_routes
--- backend/app/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from login_apis import login_routes
-from report_apis import report_routes
-from user_apis import user_routes
