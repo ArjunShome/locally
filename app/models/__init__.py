442f0a0399370c588aa142362ffe7afc014602ae
Arjun Shome <arjun_shome@epam.com>
2025-12-05 14:53:19 +0530

    updated code linting

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,7 +4,7 @@ Modules for DB models
 
 from app.models.service import Service
 from app.models.service_type import ServiceType
-from app.models.user import User
+from app.models.u_user import User
 from app.models.user_booked_service import UserBookedService
 from app.models.user_booked_service_feedback import (
     UserBookedServiceFeedback
